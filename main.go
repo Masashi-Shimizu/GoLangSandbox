ab6acfbb29187e6a3b4ecdb4499ef2f3f93ce5ee
masashi.shimizu <masashi.s0815@gmail.com>
2019-10-22 11:44:27 +0900

    add beego

--- main.go
+++ main.go
@@ -1,30 +1,11 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"path/filepath"
-	"sync"
-	"text/template"
+	_ "GoLangSandbox/routers"
+	"github.com/astaxie/beego"
 )
 
-type templateHandler struct {
-	once     sync.Once
-	filename string
-	templ    *template.Template
-}
-
-func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.request) {
-	t.once.Do(func() {
-		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
-	})
-	t.templ.Execute(w, nil)
-}
-
 func main() {
-	http.Handle("/", &templateHandler{filename: "chat.html"})
-
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal("ListenAndServe", err)
-	}
+	beego.Run()
 }
+
